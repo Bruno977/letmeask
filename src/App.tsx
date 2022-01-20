a98ca3052cd994b2daa59afec4d6e1b240ca7e03
Bruno <bruno_151997@hotmail.com>
2022-01-19 21:52:19 -0300

    Adiciona autenticacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
+import "./styles/global.scss";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
-  return <h1>Hello word</h1>;
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms/new" element={<NewRoom />} />
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import React, { ButtonHTMLAttributes } from "react";
+import "../styles/button.scss";
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+function Button(props: ButtonProps) {
+  return <button className="button" {...props} />;
+}
+
+export default Button;
